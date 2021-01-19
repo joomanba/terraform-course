d07f34de14859a771c8254897b8595dbc849db12
Jang Eui Jin <joomanba@gmail.com>
2021-01-19 15:26:44 +0900

    chore(terraform): Change AWS Region and ECR Login Script

--- module-demo/ecr-login.sh
+++ module-demo/ecr-login.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-REGION="eu-west-1"
-`aws ecr get-login --no-include-email --region ${REGION}`
+REGION="us-east-1"
+ECR_REPO="351276329499.dkr.ecr.us-east-1.amazonaws.com/my-service"
+aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ECR_REPO}
